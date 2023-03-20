db0cb64df057654fa7bc2148f2668088202d54c8
carlstep <carlstep333@me.com>
2023-03-20 10:21:37 +0700

    extract widgets

---
+++ android/app/src/main/kotlin/org/mycompany/random_number_generator/MainActivity.kt
@@ -0,0 +1,6 @@
+package org.mycompany.random_number_generator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/range_selector_form.dart
@@ -0,0 +1,77 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+typedef IntValueSetter = void Function(int value);
+
+class RangeSelectorForm extends StatelessWidget {
+  const RangeSelectorForm({
+    Key? key,
+    required this.formKey,
+    required this.minValueSetter,
+    required this.maxValueSetter,
+  }) : super(key: key);
+
+  final GlobalKey<FormState> formKey;
+
+  final IntValueSetter minValueSetter;
+  final IntValueSetter maxValueSetter;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            RangeSelectorTextFormField(
+              labelText: 'Minimum',
+              intValueSetter: minValueSetter,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            RangeSelectorTextFormField(
+              labelText: 'Maximum',
+              intValueSetter: maxValueSetter,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class RangeSelectorTextFormField extends StatelessWidget {
+  const RangeSelectorTextFormField({
+    Key? key,
+    required this.labelText,
+    required this.intValueSetter,
+  }) : super(key: key);
+
+  final String labelText;
+  final IntValueSetter intValueSetter;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      keyboardType: TextInputType.numberWithOptions(
+        decimal: false,
+        signed: true,
+      ),
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        labelText: labelText,
+      ),
+      validator: (value) {
+        if (value == null || int.tryParse(value) == null) {
+          return 'Must enter integer value';
+        } else {
+          return null;
+        }
+      },
+      onSaved: (newValue) => intValueSetter(int.parse(newValue ?? '')),
+    );
+  }
+}
